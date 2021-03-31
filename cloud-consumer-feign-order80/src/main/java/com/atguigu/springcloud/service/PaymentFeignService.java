0d65e81e4ab5067125f853b7c6237225396e8080
tanghao <17601028033@163.com>
2021-03-31 21:38:04 +0800

    业务逻辑接口+@FeignClient配置调用provider服务/Feign自带负载均衡配置项

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//Feign只需在调用方写上   对应调用方的同名方法（get、post）
+//无需@Component注解  @FeignClient自带了  你写了也不报错就是了
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService
+{
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+}
+
